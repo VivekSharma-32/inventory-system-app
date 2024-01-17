fb4c5d9d39f992dd92ccb345a51ce63f57c4fc49
Wealth Max <vivek.sharma1275@gmail.com>
2024-01-17 13:29:52 +0530

    starter project

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// connect to mongodb database and start server
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
