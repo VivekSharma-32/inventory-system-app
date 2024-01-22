5c92476e5385d1c2bd2b54eafe9d37d2342047bd
Wealth Max <vivek.sharma1275@gmail.com>
2024-01-22 15:23:42 +0530

    frontend banner, header and navbar components added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World!!!</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 };
 
