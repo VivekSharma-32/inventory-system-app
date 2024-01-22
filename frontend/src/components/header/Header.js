327492aad81212e13ab89d18c645220e3d367d1d
Wealth Max <vivek.sharma1275@gmail.com>
2024-01-22 17:05:28 +0530

    frontend ui done

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -18,6 +18,17 @@ const activeLink = ({ isActive }) => (isActive ? `${styles.active}` : "");
 
 const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
+  const [scrollPage, setScrollPage] = useState(false);
+
+  const fixedNavbar = () => {
+    if (window.scrollY > 50) {
+      setScrollPage(true);
+    } else {
+      setScrollPage(false);
+    }
+  };
+
+  window.addEventListener("scroll", fixedNavbar);
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
@@ -35,7 +46,7 @@ const Header = () => {
     </span>
   );
   return (
-    <header>
+    <header className={scrollPage ? `${styles.fixed}` : null}>
       <div className={styles.header}>
         {logo}
 
